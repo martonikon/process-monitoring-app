56c620f72fc54f35e9d7b318f5aed5429b348350
martonikon <martonikon@abv.bg>
2025-05-09 13:51:39 +0300

    function for detectin annomalies

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,10 @@ def sort_processes(data: list[dict], key: str, reverse:bool = False) -> list[dic
 def filter_processes(data: list[dict], keyword:str)->list[dict]:
     keyword=keyword.lower()
     return [stat for stat in data if keyword in stat['name'].lower()]
+
+def detect_anomalies(data, cpu_threshold=80.0, mem_threshold=80.0):
+    anomalies=[]
+    for stat in data:
+        if stat["cpu_percent"] >= cpu_threshold or stat['memory_percent'] >= mem_threshold:
+            anomalies.append(stat)
+    return anomalies
